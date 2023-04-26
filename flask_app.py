49069b7e8f9cc0064e46e65e26bbc68a12402e4c
Czenra <rybkaalex2015@mail.ru>
2023-04-26 17:09:26 +0300

    new

---
+++ flask_app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import solver
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'very_secret_super_wordle_key'
+
+data_path = os.path.join(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data'), 'words.txt')
+with open(data_path, 'r') as f:
+    word_list = f.read().splitlines()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/wordle_solver", methods=["GET", "POST"])
+def wordle_solver():
+    if request.method == "GET":
+        return render_template("wordle_solver.html")
+
+    if request.method == "POST":
+        l_ex = request.form.get("letEX")
+        l_pattern = [request.form.get("l1"), request.form.get("l2"), request.form.get("l3"), request.form.get("l4"),
+                     request.form.get("l5")]
+        y_pattern = [request.form.get("y1"), request.form.get("y2"), request.form.get("y3"), request.form.get("y4"),
+                     request.form.get("y5")]
+        return render_template("wordle_solver.html",
+                               solution=solver.solve_wordle(word_list, l_ex, l_pattern, y_pattern))
